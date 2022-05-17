a6edc267a795c7c68a9cd7f24aff30363841fee6
Archit Kumar <75969141+Architkumar29082001@users.noreply.github.com>
2022-05-17 09:43:28 +0530

    Create left rotation.cpp

---
+++ left rotation.cpp
@@ -0,0 +1,34 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <climits>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() 
+{
+    int num_of_integer,num_of_leftrotation;
+    int temp;
+    cin>>num_of_integer>>num_of_leftrotation;
+    int arr[num_of_integer];
+    for(int i=0;i<num_of_integer;i++)
+    {
+        cin>>arr[i];
+    }
+    for(int i = 0; i < num_of_leftrotation; i++){  
+        int j, first;  
+        first = arr[0];  
+      
+        for(j = 0; j < num_of_integer-1; j++){  
+            arr[j] = arr[j+1];  
+        } 
+        arr[j] = first;  
+    }
+    for(int k=0;k<num_of_integer;k++)
+    {
+        cout<<arr[k]<<" ";
+    } 
+    
+    return 0;
+}
