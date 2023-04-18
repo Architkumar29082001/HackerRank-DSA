91093b945d3f66b06e290b11056a97e58ca18c62
Archit Kumar <75969141+Architkumar29082001@users.noreply.github.com>
2023-04-18 13:46:22 +0530

    Update All Indices of Number.cpp

--- All Indices of Number.cpp
+++ All Indices of Number.cpp
@@ -1,3 +1,35 @@
+/*
+Given an array of length N and an integer x, you need to find all the indexes where x is present in the input array.
+Do this recursively. Indexing in the array starts from 0.
+Input Format
+
+Line 1 : An Integer N i.e. size of array
+Line 2 : N integers which are elements of the array, separated by spaces
+Line 3 : Integer x
+Constraints
+
+1 <= N <= 10^3
+Output Format
+
+indexes where x is present in the array (separated by space)
+Sample Input 0
+
+5
+9 8 10 8 8
+8
+Sample Output 0
+
+1 3 4
+Sample Input 1
+
+5
+9 8 10 8 8
+9
+Sample Output 1
+
+0
+*/
+
 #include <cmath>
 #include <cstdio>
 #include <vector>
